a3a56cbe9468e1589178fd96a7f69780e1321534
erangasandaruwan <eranga361881@hotmail.com>
2018-08-16 11:14:16 +0530

    Add files via upload

---
+++ BarcodeGenerator/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace BarcodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMainSymbos = new System.Windows.Forms.ComboBox();
+            this.cmbSubSymbo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHumanReadable = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(102, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(253, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Main Symbology";
+            // 
+            // cmbMainSymbos
+            // 
+            this.cmbMainSymbos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMainSymbos.FormattingEnabled = true;
+            this.cmbMainSymbos.Items.AddRange(new object[] {
+            "EAN Standards",
+            "Other"});
+            this.cmbMainSymbos.Location = new System.Drawing.Point(102, 36);
+            this.cmbMainSymbos.Name = "cmbMainSymbos";
+            this.cmbMainSymbos.Size = new System.Drawing.Size(253, 21);
+            this.cmbMainSymbos.TabIndex = 3;
+            this.cmbMainSymbos.SelectedIndexChanged += new System.EventHandler(this.cmbMainSymbos_SelectedIndexChanged);
+            // 
+            // cmbSubSymbo
+            // 
+            this.cmbSubSymbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSubSymbo.FormattingEnabled = true;
+            this.cmbSubSymbo.Items.AddRange(new object[] {
+            "EAN Standards",
+            "Other"});
+            this.cmbSubSymbo.Location = new System.Drawing.Point(102, 61);
+            this.cmbSubSymbo.Name = "cmbSubSymbo";
+            this.cmbSubSymbo.Size = new System.Drawing.Size(253, 21);
+            this.cmbSubSymbo.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Sub Symbology";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(15, 130);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(522, 171);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(102, 89);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 7;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Human Readable Text";
+            // 
+            // txtHumanReadable
+            // 
+            this.txtHumanReadable.Location = new System.Drawing.Point(128, 305);
+            this.txtHumanReadable.Name = "txtHumanReadable";
+            this.txtHumanReadable.Size = new System.Drawing.Size(409, 20);
+            this.txtHumanReadable.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 450);
+            this.Controls.Add(this.txtHumanReadable);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbSubSymbo);
+            this.Controls.Add(this.cmbMainSymbos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test Barcode Generator with NETCoreBarcode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMainSymbos;
+        private System.Windows.Forms.ComboBox cmbSubSymbo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHumanReadable;
+    }
+}
+
---
+++ BarcodeGenerator/Form1.cs
@@ -0,0 +1,101 @@
+﻿using Net.ConnectCode.Barcode;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BarcodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private void cmbMainSymbos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if(cmbMainSymbos.SelectedItem != null && !string.IsNullOrEmpty(cmbMainSymbos.SelectedItem.ToString()))
+                {
+                    cmbSubSymbo.Items.Clear();
+
+                    if(cmbMainSymbos.SelectedItem.ToString() == "EAN Standards")
+                    {
+                        var symbols = Enum.GetValues(typeof(BarcodeFonts.EANStandardsEnum))
+                                    .Cast<BarcodeFonts.EANStandardsEnum>()
+                                    .Select(val => val.ToString())
+                                    .ToList();
+                        symbols.ForEach(s => { cmbSubSymbo.Items.Add(s); });
+                    }
+                    else if (cmbMainSymbos.SelectedItem.ToString() == "Other")
+                    {
+                        var symbols = Enum.GetValues(typeof(BarcodeFonts.BarcodeEnum))
+                                    .Cast<BarcodeFonts.BarcodeEnum>()
+                                    .Select(val => val.ToString())
+                                    .ToList();
+                        symbols.ForEach(s => { cmbSubSymbo.Items.Add(s); });
+                    }
+                    else
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                BarcodeFonts barcode = new BarcodeFonts();
+
+                if (cmbMainSymbos.SelectedItem.ToString() == "EAN Standards")
+                {
+                    barcode.EANStandards = (BarcodeFonts.EANStandardsEnum)Enum.Parse(typeof(BarcodeFonts.EANStandardsEnum), cmbSubSymbo.SelectedItem.ToString());
+                }
+                else if (cmbMainSymbos.SelectedItem.ToString() == "Other")
+                {
+                    barcode.BarcodeType = (BarcodeFonts.BarcodeEnum)Enum.Parse(typeof(BarcodeFonts.BarcodeEnum),cmbSubSymbo.SelectedItem.ToString());
+                }
+
+                barcode.CheckDigit = BarcodeFonts.YesNoEnum.No;
+                barcode.Data = txtInput.Text;
+                //if (checkBox1.Checked)
+                //    barcode.CheckDigit = BarcodeFonts.YesNoEnum.Yes;
+                barcode.encode();
+                txtHumanReadable.Text = barcode.HumanText;
+                txtOutput.Text = barcode.EncodedData;
+                Font font = new Font("CCode39_S3_Trial", 30);
+                txtOutput.Font = font;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
